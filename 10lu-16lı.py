b733de379434f2d0f92981bb0f0e6545eca5dc01
pascia <58901890+pascia@users.noreply.github.com>
2019-12-15 11:13:38 +0300

    10'lu sayısı düzenini 16'lı sayı düzenine çevirme
    Pascia'dan 10'lu sayısı düzenini 16'lı sayı düzenine çevirme uygulaması. (decimal to hexdecimal)

---
+++ 10lu-16lı.py
@@ -0,0 +1,13 @@
+while 1==1:
+    girissayisi = int(input("Bir sayı girin, 16'lı sisteme dönüştürelim"))
+    alfabe = "ABCDEF"
+    cikissayisi = ""
+
+    while girissayisi != 0:
+        if (girissayisi%16 >= 10): # Sayıyı 16'ya böl. Kalanı en sağa satıra yaz.
+            cikissayisi = alfabe[(girissayisi%16)-10] + cikissayisi #Kalan 9'dan fazla ise alfabe'deki bir harfi kullan.
+        else: #Kalan 9'dan az ise, doğrudan çıkış sayısı'nın başına ekle
+            cikissayisi = str(girissayisi%16)+cikissayisi
+        girissayisi//=16 # Her şekilde sayıyı 16'ya kalansız böl. Bir sonraki işlemde tekrar kullan
+
+    print("Sayınız çevrildi:", cikissayisi)
